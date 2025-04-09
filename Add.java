e955a162e0d87cf73c403c6c59ef180e7b2a53d5
vktech04 <kulkarnivaishu9504@gmail.com>
2025-04-09 15:23:22 +0530

    saved in main

---
+++ Add.java
@@ -0,0 +1,10 @@
+package JAVA3;
+
+public class Add{
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 5;
+    int sum = a + b;
+    System.out.println("The Addition is : "+sum);
+    } 
+}
